6b875041964f46f1ea8b8371330022924002908f
rafaelkellows <rafaelkellows@hotmail.com>
2017-05-02 09:20:36 -0300

    Upload Store First Commit

---
+++ inc/head.php
@@ -0,0 +1,61 @@
+	<head>
+		<title>Rodrigues Sports - artigos esportivos - <?php print $page_title; ?></title>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"> 
+		<meta name="viewport" content="width=device-width,user-scalable=no">
+		<meta name="Keywords" content="Spatula, Presentes, Marca, Bolacha, Chaveiro, Xícara, Copo, Caneca, Garrafa, Térmica, Plástica, Porta, Retrato, Sandália, Chinelo, Plástico, Descartável, Linha, Coleção, Bolsa, Almofada, Capa, Celular, Brinde, Acessório, Personaliza">
+		<meta name="Description" content="Spatula - Presentes que marcam">
+		
+		<link rel="apple-touch-icon" sizes="57x57" href="images/icons/apple-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="images/icons/apple-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="images/icons/apple-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="images/icons/apple-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="images/icons/apple-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="images/icons/apple-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="images/icons/apple-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="images/icons/apple-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="images/icons/apple-icon-180x180.png">
+		<link rel="icon" type="image/png" sizes="192x192"  href="images/icons/android-icon-192x192.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="images/icons/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="96x96" href="images/icons/favicon-96x96.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="images/icons/favicon-16x16.png">
+		<link rel="manifest" href="images/icons/manifest.json">
+		<meta name="msapplication-TileColor" content="#ffffff">
+		<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+		<meta name="theme-color" content="#ffffff">
+
+		<link rel="stylesheet" type="text/css" href="css/stylesheet.css" media="screen" />
+		<link rel="stylesheet" type="text/css" href="css/styles.css" media="screen" />	  
+		<link rel="stylesheet" type="text/css" href="css/font-awesome.css" media="screen" />
+
+	    <!-- Owl Carousel Assets -->
+	    <link href="css/owl-carousel/owl.carousel.css" rel="stylesheet">
+	    <link href="css/owl-carousel/owl.theme.css" rel="stylesheet">
+
+	    <script src="js/jquery-1.11.3.min.js"></script> 
+		<script src="js/jquery.maskedinput.min.js" type="text/javascript"></script>
+		<script src="js/jquery.validate.js" type="text/javascript"></script>
+		<script src="js/additional-methods.min.js"></script>
+	    <script src="js/owl-carousel/owl.carousel.min.js"></script>
+		<script src="js/jquery.cookie.js" type="text/javascript"></script>
+		<!--script src="https://stc.sandbox.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.lightbox.js" type="text/javascript"></script-->
+	    <script src="js/scripts.js" type="text/javascript"></script>
+
+		<!--[if lt IE 9]>
+		  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<!--
+		<script type="text/javascript">var switchTo5x=true;</script>
+		<script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
+		<script type="text/javascript">stLight.options({publisher: "c40dcd16-0f3e-4d40-81b5-237a219a994f", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-77107607-1', 'auto');
+		  ga('send', 'pageview');
+
+		</script-->
+		
+	</head>
---
+++ inc/nav.php
@@ -0,0 +1,11 @@
+		<nav class="hide">
+			<a class="fa fa-list-ul" href="javascript:void(0);"></a>
+			<a href="./">Home</a>
+			<a href="empresa.php">A Empresa</a>
+			<a href="corporativo.php">Corporativo</a>
+			<a href="produtos.php">Nossos Produtos</a>
+			<a href="orcamento.php">Faça seu Orçamento</a>
+			<!--a href="ethics.php">Nossa Filosofia</a>
+			<a href="social.php">Social</a-->
+			<a href="contato.php">Contatos</a> 
+		</nav>
